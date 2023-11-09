c575100b486b10a2325be94fdeda63d7a168ac79
Freewind <nowindlee@gmail.com>
2023-11-09 21:57:35 +0800

    fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,23 @@
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackModifyBeforeEmittingPlugin from './src/webpack-modify-before-emiting-plugin';
 
 module.exports = {
-  mode: 'development',
-  entry: './entry.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new WebpackModifyBeforeEmittingPlugin({
-      enable: true,
-      modifyFn: (assetSources) => {
-        for (const assetName in assetSources) {
-          const content = assetSources[assetName];
-          assetSources[assetName] = content.replace('---placeholder---', '(modified in webpack plugin)');
-        }
-        return assetSources;
-      }
-    })
-  ]
+    mode: 'development',
+    entry: './entry.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new WebpackModifyBeforeEmittingPlugin({
+            enable: true,
+            modifyFn: (assetSources) => {
+                for (const assetName in assetSources) {
+                    const content = assetSources[assetName];
+                    assetSources[assetName] = content.replace('---placeholder---', '(modified in webpack plugin)');
+                }
+                return assetSources;
+            }
+        })
+    ]
 }
